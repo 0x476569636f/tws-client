9d9d6a426dd8b4d6941ac39bf03c57bc41372c6d
Irvan Pramana Putra <darknet1255@gmail.com>
2024-11-19 12:52:38 +0700

    feat: update Input component with forwardRef and add optional onBlur prop

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { View, TextInput, useColorScheme } from 'react-native';
 import { cn } from '~/lib/cn';
 
-type inputProps = {
+type InputProps = {
   containerStyle?: string;
   icon?: React.ReactNode;
-  inputRef?: React.RefObject<TextInput>;
+  rightIcon?: React.ReactNode;
   placeholder?: string;
   onChangeText?: (value: string) => void;
   secureTextEntry?: boolean;
   multiline?: boolean;
   numberOfLines?: number;
   value?: string;
+  // Add optional onBlur prop
+  onBlur?: () => void;
 };
 
-const Input: React.FC<inputProps> = (props) => {
+const Input = forwardRef<TextInput, InputProps>((props, ref) => {
   const colorScheme = useColorScheme();
   const isDarkColorScheme = colorScheme === 'dark';
+
+  const { containerStyle, icon, rightIcon, onBlur, ...inputProps } = props;
 
   return (
     <View
       className={cn(
-        `h-12 flex-row items-center gap-2 rounded-md border border-gray-400 px-4 ${props.containerStyle}`
+        `h-12 flex-row items-center gap-2 rounded-md border border-gray-400 px-4 ${containerStyle}`
       )}>
-      {props.icon && props.icon}
+      {icon && icon}
       <TextInput
-        style={{ flex: 1, color: isDarkColorScheme ? 'white' : 'black' }}
+        ref={ref}
+        style={{
+          flex: 1,
+          color: isDarkColorScheme ? 'white' : 'black',
+          fontFamily: 'Inter_400Regular',
+        }}
         placeholderTextColor={isDarkColorScheme ? 'white' : 'black'}
-        ref={props.inputRef}
-        value={props.value}
-        multiline={props.multiline}
-        numberOfLines={props.numberOfLines}
-        {...props}
+        onBlur={onBlur}
+        {...inputProps}
       />
+      {rightIcon && rightIcon}
     </View>
   );
-};
+});
+
+Input.displayName = 'Input';
 
 export default Input;
